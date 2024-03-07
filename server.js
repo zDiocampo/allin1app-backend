6512c1a94b88720ba730abd286938129b50af443
Zandro Diocampo <109254284+zDiocampo@users.noreply.github.com>
2024-03-07 22:44:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.delete('/api/comments/:id', async (req, res) => {
     }
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
