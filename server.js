aa5bde40e1945ea79461e7893232c8b7f62b1c21
Zandro Diocampo <109254284+zDiocampo@users.noreply.github.com>
2024-03-10 19:55:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'https://threein1.onrender.com/'
 }));
 app.options('*', cors());
 app.use(bodyParser.json());
